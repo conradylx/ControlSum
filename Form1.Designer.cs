5a9bf7561ab3f7b5d3741de89050ce31fa5f3dab
conradylx <50596942+conradylx@users.noreply.github.com>
2020-04-28 18:08:37 +0200

    Control sum

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ControlSum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nrbInput = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.nrbOutput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.convert = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.nrbInput);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(455, 90);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Rachunek w formacie NRB";
+            // 
+            // nrbInput
+            // 
+            this.nrbInput.Location = new System.Drawing.Point(25, 48);
+            this.nrbInput.Name = "nrbInput";
+            this.nrbInput.Size = new System.Drawing.Size(406, 22);
+            this.nrbInput.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.nrbOutput);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(455, 88);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // nrbOutput
+            // 
+            this.nrbOutput.Location = new System.Drawing.Point(25, 48);
+            this.nrbOutput.Name = "nrbOutput";
+            this.nrbOutput.Size = new System.Drawing.Size(406, 22);
+            this.nrbOutput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(126, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Rachunek z sumą kontrolną";
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(475, 13);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(92, 177);
+            this.convert.TabIndex = 2;
+            this.convert.Text = "Konwertuj";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 208);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox nrbInput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox nrbOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button convert;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlSum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+            //nrbOutput.Text = nrbInput.Text;
+            Regex validation = new Regex("^[1-9]{24}$");
+            Boolean isValid = validation.IsMatch(nrbInput.Text);
+
+            if (!isValid)
+            {
+                MessageBox.Show("Wprowadź numer rachunku w formacie NRB [24 cyfry]");
+            }
+            else
+            {
+                string number = nrbInput.Text + Convert.ToByte('P') + Convert.ToByte('L');
+                int modulo = 0;
+
+                foreach (char znak in number)
+                    modulo = (10 * modulo + int.Parse(znak.ToString())) % 97;
+                modulo = 98 - modulo;
+
+                nrbOutput.Text = string.Format("{0}{1}", modulo, nrbInput.Text);
+            }
+        }
+    }
+}
